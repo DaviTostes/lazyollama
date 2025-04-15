5c5eaac53da4a3da907617c7b5e3fbb257816ec6
Davi Tostes <davisiqueira591@gmail.com>
2025-04-15 00:53:05 -0300

    refactor: modularizing startChatLoop function

--- view/chat.go
+++ view/chat.go
@@ -11,10 +11,29 @@ import (
 )
 
 func AddNewChat() {
-	scanner := bufio.NewScanner(os.Stdin)
 	firstMessage := true
 	var chatId int64
 
+	startChatLoop(firstMessage, chatId)
+}
+
+func OpenChat(chatId int64) {
+	sqliteClient, err := db.NewSQLiteClient()
+	if err != nil {
+		panic(err)
+	}
+
+	messages, _ := sqliteClient.FetchMessages(chatId)
+
+	for _, m := range messages {
+		fmt.Println(m.Content)
+	}
+
+	startChatLoop(false, chatId)
+}
+
+func startChatLoop(firstMessage bool, chatId int64) {
+	scanner := bufio.NewScanner(os.Stdin)
 	sqliteClient, err := db.NewSQLiteClient()
 	if err != nil {
 		panic(err)
@@ -47,56 +66,6 @@ func AddNewChat() {
 
 			firstMessage = false
 		}
-
-		userMessage := model.Message{
-			Sender:    "user",
-			Content:   scanner.Text(),
-			CreatedAt: time.Now().String(),
-			ChatId:    chatId,
-		}
-		go sqliteClient.CreateMessage(userMessage)
-
-		modelMessage := model.Message{
-			Sender:    "model",
-			Content:   gen.Response,
-			CreatedAt: time.Now().String(),
-			ChatId:    chatId,
-		}
-		go sqliteClient.CreateMessage(modelMessage)
-	}
-}
-
-func OpenChat(chatId int64) {
-	scanner := bufio.NewScanner(os.Stdin)
-	sqliteClient, err := db.NewSQLiteClient()
-	if err != nil {
-		panic(err)
-	}
-
-	messages, _ := sqliteClient.FetchMessages(chatId)
-
-	for _, m := range messages {
-		fmt.Println(m.Content)
-	}
-
-	for {
-		fmt.Print("> ")
-		scanner.Scan()
-		err := scanner.Err()
-		if err != nil {
-			panic(err)
-		}
-
-		ollama := ollama.Ollama{
-			Model: os.Getenv("DEFAULT_MODEL"),
-		}
-
-		gen, err := ollama.Generate(scanner.Text())
-		if err != nil {
-			panic(err)
-		}
-
-		fmt.Print("\n")
 
 		userMessage := model.Message{
 			Sender:    "user",
